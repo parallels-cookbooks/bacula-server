b81897fc3198d260c708cf534ecb50b4152a0817
Pavel Yudin <pyudin@parallels.com>
2015-12-08 17:27:39 +0300

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pyudin@parallels.com'
 license          'Apache 2.0'
 description      'Installs/Configures bacula-server'
 long_description 'Installs/Configures bacula-server'
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'postgresql'
 
