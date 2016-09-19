bf9872836c31738aa581176bf1835c9180286f52
Pavel Yudin <pyudin@parallels.com>
2016-09-19 15:01:21 +0300

    Bump version to v0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pyudin@parallels.com'
 license          'Apache 2.0'
 description      'Installs/Configures bacula-server'
 long_description 'Installs/Configures bacula-server'
-version          '0.1.2'
+version          '0.1.3'
 
 source_url 'https://github.com/parallels-cookbooks/bacula-server'
 issues_url 'https://github.com/parallels-cookbooks/bacula-server/issues'
